dba8cdf28f195476c2b91bd72fcb9dbb8cdfd0c3
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-28 14:14:37 -0500

    add random_color and modify method calls to set with and color of the line

--- random-walk.py
+++ random-walk.py
@@ -1,19 +1,27 @@
+import turtle
 from turtle import Turtle, Screen
 from data import colors
 import random
 
+turtle.colormode(255)
 screen = Screen()
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 tim = Turtle()
 tim.shape('circle')
-tim.width(6)
+tim.pensize(6)
 tim.speed(5)
 
 width = screen.window_width() // 2
 height = screen.window_height() // 2
 
 for _ in range(50):
-    tim.color(random.choice(colors))
+    tim.pencolor(random_color())
     tim.setx(random.randint(-width, width))
     tim.sety(random.randint(-height, height))
 
