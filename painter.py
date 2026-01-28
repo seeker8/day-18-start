5d41b40ea32d2a66aa3ae717c264d99a5f541f2f
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-28 18:35:21 -0500

    comment code to extract colors, add instructions for final challenge and update helpers with the rgb colors got from the image

--- painter.py
+++ painter.py
@@ -1,10 +1,17 @@
-import colorgram
+# this code was used to generate the rgb color list in helpers
+#import colorgram
+# colors = colorgram.extract('spot.png', 30)
+# normalized_colors = []
 
-colors = colorgram.extract('spot.png', 10)
-normalized_colors = []
+# for color in colors:
+#     r,g,b = color.rgb
+#     normalized_colors.append((r,g,b))
+#
+# print(normalized_colors)
 
-for color in colors:
-    r,g,b = color.rgb
-    normalized_colors.append((r,g,b))
+from helpers import rbg_colors
 
-print(normalized_colors)
+
+# canvas 10 x 10 dots
+# each dot should be size 20
+# separated by 50
