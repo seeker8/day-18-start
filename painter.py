1a7c1b992635eb160cd0fca44bc93fb2bfca0207
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-28 21:57:06 -0500

    add my solution to drawing dots

--- painter.py
+++ painter.py
@@ -2,6 +2,7 @@
 #import colorgram
 # colors = colorgram.extract('spot.png', 30)
 # normalized_colors = []
+import turtle
 
 # for color in colors:
 #     r,g,b = color.rgb
@@ -10,8 +11,24 @@
 # print(normalized_colors)
 
 from helpers import rbg_colors
+from turtle import Turtle, Screen
+import random
 
 
 # canvas 10 x 10 dots
 # each dot should be size 20
 # separated by 50
+turtle.colormode(255)
+screen = Screen()
+jimi = Turtle()
+jimi.speed("fastest")
+for y in range(10):
+    jimi.goto(0, y * 50)
+    for _ in range(10):
+        jimi.color(random.choice(rbg_colors))
+        jimi.dot(20)
+        jimi.penup()
+        jimi.forward(50)
+
+
+screen.exitonclick()
